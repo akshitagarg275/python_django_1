5623ff730ae5aaa6a290b282fe36d14b79520182
akshitagarg275 <akshitagarg275@gmail.com>
2022-06-25 11:49:55 +0530

    day 22

---
+++ 10JS/01hello.js
@@ -0,0 +1,84 @@
+// console.log("Hello World")
+
+// const pi = 3.21
+
+// console.log(pi)
+
+
+// let a 
+
+// console.log(a)
+
+// age = 12
+
+// if (age >= 18){
+//     console.log("You can drive")
+// }else{
+//     console.log("You cannot drive")
+// }
+
+// role="admin"
+
+// switch(role){
+//     case "admin":
+//         console.log("ADMIN")
+//         break;
+//     case "user":
+//         console.log("USER")
+//         break;
+//     case "sub-admin":
+//         console.log("SUB-ADMIN")
+//         break
+//     default:
+//         console.log("Another user")
+// }
+
+
+
+// for(let i=0 ; i<5 ; i++){
+//     console.log(i)
+// }
+
+
+stu = {"fname":"John", "lname":"doe"}
+
+// for (k in stu){
+//     console.log(k)
+// }
+
+// nums = [1,2,3,4,5,6]
+// It provides us the value
+// for (i of nums){
+//     console.log(i)
+// }
+
+// It provides us the index number
+// for (i in nums){
+//     console.log(i)
+// }
+
+
+// function greet(){
+//     console.log("I am in a function")
+// }
+
+
+// greet = function () {
+//     console.log("I am in a function")
+// }
+
+// greet = () => {
+//     console.log("I am in arrow function")
+// }
+
+// greet()
+
+
+// console.log(3=="3")
+// console.log(3==="3")
+
+
+// h = document.getElementById("h1")
+// h.innerHTML = "Javascript"
+// console.log(h)
+
---
+++ 11django/01hello_world/helloworld/namaste/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+# 127.0.0.1:8000
+urlpatterns = [
+    path('',views.home , name = "home"),
+]
