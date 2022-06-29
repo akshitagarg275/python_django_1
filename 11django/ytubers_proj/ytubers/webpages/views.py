43eb91500afbabcc46d08dc281677d23245f7125
akshitagarg275 <akshitagarg275@gmail.com>
2022-06-29 09:03:57 +0530

    day 24 25

---
+++ 11django/ytubers_proj/ytubers/webpages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render (request , 'webpages/main.html')
+
+def contact(request):
+    return render (request , 'webpages/contact.html')
+
+def about(request):
+    return render (request , 'webpages/about.html')
+
+def services(request):
+    return render (request , 'webpages/services.html')
+
+def dashboard(request):
+    return render (request , 'webpagesdashboard/.html')
