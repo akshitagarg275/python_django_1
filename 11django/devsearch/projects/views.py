ace3c3cacac384135bbb0c1777194694a9de2bbe
akshitagarg275 <akshitagarg275@gmail.com>
2022-07-28 22:42:15 +0530

    day 40

--- 11django/devsearch/projects/views.py
+++ 11django/devsearch/projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def projects(request):
+    return render(request,'projects/projects.html')
+
+
+def single_project(request , pk):
+    return render(request, 'projects/single_project.html')
