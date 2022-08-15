73c3508caf7c8389cd2b31cc27f94f446bc9ae53
akshitagarg275 <akshitagarg275@gmail.com>
2022-08-15 21:54:57 +0530

    day 43

--- 11django/devsearch/users/admin.py
+++ 11django/devsearch/users/admin.py
@@ -1,6 +1,7 @@
 import imp
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Skill
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Skill)
