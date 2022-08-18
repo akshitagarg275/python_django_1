12096bdecdaa46c462b2e3434a53e0c54459071a
akshitagarg275 <akshitagarg275@gmail.com>
2022-08-18 21:43:53 +0530

    day 47

--- 11django/devsearch/projects/views.py
+++ 11django/devsearch/projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from .forms import ProjectForm
 from .models import Project
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def projects(request):
@@ -18,7 +19,7 @@ def single_project(request , pk):
     }
     return render(request, 'projects/single_project.html',context)
 
-
+@login_required(login_url='login')
 def createProject(request):
     form = ProjectForm()
     if request.method == 'POST':
@@ -32,7 +33,7 @@ def createProject(request):
     return render(request , 'projects/project_form.html',context)
 
 
-
+@login_required(login_url='login')
 def updateProject(request , pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -46,7 +47,7 @@ def updateProject(request , pk):
     context = {'form':form}
     return render(request , 'projects/project_form.html',context)
 
-
+@login_required(login_url='login')
 def deleteProject(request,pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
